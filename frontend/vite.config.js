7b7afa4fcd2567ffa2619102bac7594763daf1e6
Priyanshu Gautam <134134060+PriyanshuG24@users.noreply.github.com>
2025-04-19 09:06:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-   server: {
-        proxy: {
-            '/api': {
-                target: 'https://servers-monitor-hk7v.vercel.app',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://servers-monitor-hk7v.vercel.app',
+        changeOrigin: true,
+        secure: false
+      }
     }
-})
+  }
+});
