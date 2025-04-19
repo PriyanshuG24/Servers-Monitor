4df079a24a88c9894d47707ee50de8eaa2cfcf21
Priyanshu Gautam <134134060+PriyanshuG24@users.noreply.github.com>
2025-04-19 12:22:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,25 +12,29 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://servers-monitor.vercel.app",
-  "http://localhost:5173",
+  "https://servers-monitor.vercel.app/", 
+  "http://localhost:5173", 
 ];
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         console.log("Blocked origin:", origin);
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+
+app.options("*", cors());
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 // Add OPTIONS preflight handling
 // app.options("*", cors());
