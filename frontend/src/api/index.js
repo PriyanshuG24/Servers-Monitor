f60949e42adc6babe07fc30467b2dcd713d3fa7b
priyanshu <priyanshu10503@gmail.com>
2025-04-20 12:21:09 +0530

    update the index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,17 +1,17 @@
 export const getAlerts = async () => {
-    const res = await fetch('/api/alerts');
+    const res = await fetch('https://servers-monitor-idq2.vercel.app/api/alerts');
     if (!res.ok) throw new Error('Failed to fetch alerts');
     return res.json();
   };
   
   export const getMetrics = async () => {
-    const res = await fetch('/api/metrics');
+    const res = await fetch('https://servers-monitor-idq2.vercel.app/api/metrics');
     if (!res.ok) throw new Error('Failed to fetch metrics');
     return res.json();
   };
   
   export const getServers = async () => {
-    const res = await fetch('/api/servers');
+    const res = await fetch('https://servers-monitor-idq2.vercel.app/api/servers');
     if (!res.ok) throw new Error('Failed to fetch servers');
     return res.json();
   };
